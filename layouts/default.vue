3f6716ebfd69ee7cd5f055ea3d222614ed01c16c
Rafalovich <r3179407@gmail.com>
2025-08-04 16:18:21 +0300

    Creating a basic sidebar, and composing folders is required

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="layout rtl">
+        <sidbar/>
+        <div class="content">
+           <NuxtPage />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Sidbar from '~/components/sidebar.vue'
+</script>
+
+<style scoped>
+.layout {
+    display: flex;
+    padding: 1rem;
+    overflow-y: auto;
+}
+.rtl {
+    direction: rtl;
+}
+</style>
